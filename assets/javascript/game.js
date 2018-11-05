b470a2789db5ca3830c94b763859466f065a06de
variable56 <variable56@gmail.com>
2018-11-04 21:27:06 -0700

    Added win tracker (not working yet) and guess countdown

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,12 +38,16 @@
         var randomNum = Math.floor(Math.random() * 28);
         var hiddenOption = options[randomNum];
         var guessStatus = [];
+        var wins = 0;
+        var remGuesses = 15;
+        document.getElementById("wins").innerHTML = ("Wins: " + wins);
+        document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
 
         console.log(hiddenOption);
             for (var i = 0; i < hiddenOption.length; i++) {
                 guessStatus.push("-");
             }
-
+        //takes all the commas out of the guessStatus array
         document.getElementById("selection").innerHTML = guessStatus.join(' ');
 
         //there needs to be a place to display all of the user guesses and a bank of answers displayed everytime the user makes a guess
@@ -55,6 +59,11 @@
             toLowerCase(); 
             userGuesses.push(userInput);
             document.getElementById("guesses").innerHTML = userGuesses;
+            document.getElementById("welcometext").innerHTML = '';
+
+        //each time a user makes a guess, the number of guesses left needs to decrease by 1
+            remGuesses--;
+            document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
 
         //Replace values of "-" in guessStatus every time a correct letter is guessed
 
@@ -71,6 +80,5 @@
                 }
             }
         }
-
-        //each time a user makes a guess, the number of guesses left needs to decrease by 1
+        
         //if the user guesses all the letters of the word within the guesses, increase the win counter by 1
