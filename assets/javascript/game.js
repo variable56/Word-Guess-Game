9582828c704d3e112be04aa25533d347feb6774f
variable56 <variable56@gmail.com>
2018-11-04 22:03:05 -0700

    working countdown guesses and game over screen with reset, still need working win screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,7 +61,7 @@
             document.getElementById("guesses").innerHTML = userGuesses;
             document.getElementById("welcometext").innerHTML = '';
 
-        //each time a user makes a guess, the number of guesses left needs to decrease by 1
+        //each time a user makes a guess, the number of guesses left needs to decrease by 1 if they use all guesses they lose.
             remGuesses--;
             document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
 
@@ -69,16 +69,38 @@
 
             for (var i=0; i < hiddenOption.length; i++) {
                 if (hiddenOption.charAt(i) === userInput) { 
-                console.log(hiddenOption.charAt(i));
-                console.log(userInput);
             
                     if (i !== -1) {
                     guessStatus[i] = userInput;
                     document.getElementById("selection").innerHTML = guessStatus.join(' '); 
                     }
-
+                    // var solution = (guessStatus.join())
+                    // if (solution = hiddenOption) {
+                    // console.log(solution)
+                    // console.log(typeof(solution))
+                    // wins++;
+                    // }
                 }
             }
+
+            //resets the game if they lose
+            if (remGuesses < 0) {
+                alert("game over, click \"ok\" then press any key to start over")
+                randomNum = Math.floor(Math.random() * 28);
+                hiddenOption = options[randomNum];
+                guessStatus = [];
+                wins = 0;
+                remGuesses = 15;
+                document.getElementById("wins").innerHTML = ("Wins: " + wins);
+                document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
+                userGuesses = []
+
+                console.log(hiddenOption);
+                    for (var i = 0; i < hiddenOption.length; i++) {
+                        guessStatus.push("-");
+                    }
+                }
         }
+
         
         //if the user guesses all the letters of the word within the guesses, increase the win counter by 1
