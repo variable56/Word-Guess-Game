43a3a918aa20a06b324c66423c57f28d9e54da8f
variable56 <variable56@gmail.com>
2018-11-04 23:47:40 -0700

    styled but needs game over reset to stop looping, and win condition created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
        
-        //put all the necessary information for the game into an object
+        //create array for words to guess.
         var 
         options = ["dementor",
         "quidditch",
@@ -50,7 +50,7 @@
         //takes all the commas out of the guessStatus array
         document.getElementById("selection").innerHTML = guessStatus.join(' ');
 
-        //there needs to be a place to display all of the user guesses and a bank of answers displayed everytime the user makes a guess
+        //there needs to be a place to display all of the user guesses and a bank of guesses displayed everytime the user makes a guess
         
         var userGuesses = [];
 
@@ -61,7 +61,7 @@
             document.getElementById("guesses").innerHTML = userGuesses;
             document.getElementById("welcometext").innerHTML = '';
 
-        //each time a user makes a guess, the number of guesses left needs to decrease by 1 if they use all guesses they lose.
+        //each time a user makes a guess, the number of guesses left needs to decrease by 1.
             remGuesses--;
             document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
 
@@ -83,24 +83,31 @@
                 }
             }
 
-            //resets the game if they lose
-            if (remGuesses < 0) {
-                alert("game over, click \"ok\" then press any key to start over")
+            //if they use all guesses they lose & the game resets
+            if (remGuesses < 1) {
+                
+                document.onkeyup = function reset() {
+                userGuesses = []
+                guessStatus = [];
                 randomNum = Math.floor(Math.random() * 28);
                 hiddenOption = options[randomNum];
-                guessStatus = [];
-                wins = 0;
-                remGuesses = 15;
-                document.getElementById("wins").innerHTML = ("Wins: " + wins);
-                document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
-                userGuesses = []
-
                 console.log(hiddenOption);
                     for (var i = 0; i < hiddenOption.length; i++) {
                         guessStatus.push("-");
                     }
+                document.getElementById("selection").innerHTML = guessStatus.join(' ');
+                remGuesses = 15;
+                document.getElementById("remguesses").innerHTML = ("Guesses Remaining: " + remGuesses);
+                document.getElementById("guesses").innerHTML = userGuesses;
+                alert("Game Over, He who must not be named has prevailed...")
                 }
-        }
-
+            }
+             //if the user guesses all the letters of the word within the guesses, increase the win counter by 1
+            
+                // if (hiddenOption = (guessStatus.join(''))) {
+                //     wins++;
+                // }
         
-        //if the user guesses all the letters of the word within the guesses, increase the win counter by 1
+        
+        
+        }
